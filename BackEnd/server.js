9204f1da1803d385a0c9bb8b5a43d9b2c0111474
Jose Italo <122283414+JoseItaloP@users.noreply.github.com>
2025-02-05 09:19:14 -0300

    Back end atualizado
    preparando conexão com railway

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const fastify = require("fastify")({ logger: true });
 const cords = require ('@fastify/cors')
 fastify.register(cords)
@@ -7,7 +8,7 @@ fastify.register(require('./route/user'))
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: process.env.PORT_CONNECTION });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
