a116df05d8b44e417ff6d41a2fd1e329f4e21d01
Jose Italo <122283414+JoseItaloP@users.noreply.github.com>
2025-02-05 15:45:16 -0300

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,30 +1,18 @@
 require('dotenv').config()
-import Fastify  from 'fastify';
-import cords from '@fastify/cors'
-import TaskRoute from './route/task'
-import UserRoute from './route/user';
-const app = Fastify({
-  logger: true,
-})
-app.register(cords)
-app.register(TaskRoute);
-app.register(UserRoute)
+const fastify = require("fastify")({ logger: true });
+const cords = require ('@fastify/cors')
+fastify.register(cords)
+fastify.register(require("./route/task"));
+fastify.register(require('./route/user'))
 
-app.get('/hello',()=>{
-  return 'hello'
-})
 
-app.listen({port: process.env.PORT_CONNECTION}).then(()=>{
-  console.log('conected')
-})
+const start = async () => {
+  try {
+    await fastify.listen({ port: process.env.PORT_CONNECTION });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
 
-// const start = async () => {
-//   try {
-//     await fastify.listen({ port: process.env.PORT_CONNECTION });
-//   } catch (err) {
-//     fastify.log.error(err);
-//     process.exit(1);
-//   }
-// };
-
-// start();
+start();
