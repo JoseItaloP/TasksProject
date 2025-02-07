74ca0c4317a52f7f3140f1c23af2890323ca2c45
Jose Italo <122283414+JoseItaloP@users.noreply.github.com>
2025-02-07 16:34:06 -0300

    Update index.js

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -5,12 +5,7 @@ fastify.register(require('./route/user'))
 fastify.register(require('./route/task'))
 
 fastify.get('/', async (req, reply) => {
-  return reply.send('hello')
-})
 
-export default async function handler(req, reply) {
-  await fastify.ready()
- 
   reply.setHeader('Access-Control-Allow-Origin', '*')
   reply.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
 
@@ -18,7 +13,18 @@ export default async function handler(req, reply) {
     res.status(200).end()
     return
   }
+  return reply.send('hello')
+})
 
+export default async function handler(req, reply) {
+  reply.setHeader('Access-Control-Allow-Origin', '*')
+  reply.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  await fastify.ready()
   fastify.server.emit('request', req, reply)
 }
 
