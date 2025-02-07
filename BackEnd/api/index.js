51cbe82999bb0ea02eda8cfb647db4065a605cfc
Jose Italo <122283414+JoseItaloP@users.noreply.github.com>
2025-02-07 16:04:33 -0300

    Update index.js

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -10,14 +10,10 @@ fastify.get('/', async (req, reply) => {
 
 export default async function handler(req, reply) {
   await fastify.ready()
-  reply.setHeader('Access-Control-Allow-Credentials', true)
+ 
   reply.setHeader('Access-Control-Allow-Origin', '*')
-
   reply.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  reply.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+  
   fastify.server.emit('request', req, reply)
 }
 
