59ac9a9ca999c49a7398284f6a9deefcd032dac9
Jose Italo <122283414+JoseItaloP@users.noreply.github.com>
2025-02-07 15:51:14 -0300

    Update index.js

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -1,11 +1,8 @@
-// import Fastify from 'fastify'
-// import UserRoute from './route/user'
-// import TaskRoute from './route/task'
-// import cors from '@fastify/cors'
+
 const fastify = require('fastify')({logger: true})
+fastify.register(require('@fastify/cors'))
 fastify.register(require('./route/user'))
 fastify.register(require('./route/task'))
-// fastify.register(require('@fastify/cors'))
 
 fastify.get('/', async (req, reply) => {
   return reply.send('hello')
