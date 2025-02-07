4e3316c824ab2f07fb834d04da9bf4ca1d0a026e
Jose Italo <122283414+JoseItaloP@users.noreply.github.com>
2025-02-07 15:56:01 -0300

    Update index.js

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -1,6 +1,6 @@
 
 const fastify = require('fastify')({logger: true})
-fastify.register(require('@fastify/cors'))
+// fastify.register(require('@fastify/cors'))
 fastify.register(require('./route/user'))
 fastify.register(require('./route/task'))
 
@@ -10,6 +10,14 @@ fastify.get('/', async (req, reply) => {
 
 export default async function handler(req, reply) {
   await fastify.ready()
+  reply.setHeader('Access-Control-Allow-Credentials', true)
+  reply.setHeader('Access-Control-Allow-Origin', '*')
+
+  reply.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  reply.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   fastify.server.emit('request', req, reply)
 }
 
