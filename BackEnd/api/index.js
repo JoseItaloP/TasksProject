0b14bfcf6e60a746a15ebc773c4724f031689f8e
Jose Italo <122283414+JoseItaloP@users.noreply.github.com>
2025-02-07 16:45:50 -0300

    update cors

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -4,6 +4,7 @@ fastify.register(require('@fastify/cors'), {
   origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
+  credentials: true, // Permite cookies e autenticação
 });
 fastify.register(require('./route/user'))
 fastify.register(require('./route/task'))
@@ -12,9 +13,8 @@ fastify.get('/', async (req, reply) => {
   return reply.send('hello')
 })
 
-export default async function handler(req, reply) {
-  
-  await fastify.ready()
-  fastify.server.emit('request', req, reply)
-}
+module.exports = async (req, res) => {
+  await fastify.ready();
+  fastify.server.emit('request', req, res);
+};
 
