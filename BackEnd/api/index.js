14f3139f872f0689e025c3ca4289f5393de4f5a1
Jose Italo <122283414+JoseItaloP@users.noreply.github.com>
2025-02-07 11:17:48 -0300

    Update index.js

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -1,22 +1,18 @@
-import Fastify from 'fastify'
-import UserRoute from './route/user'
-import TaskRoute from './route/task'
-import cors from '@fastify/cors'
+// import Fastify from 'fastify'
+// import UserRoute from './route/user'
+// import TaskRoute from './route/task'
+// import cors from '@fastify/cors'
+const fastify = require('fastify')({logger: true})
+fastify.register(require('@fastify/cors'))
+fastify.register(require('./route/user'))
+fastify.register(require('./route/task'))
 
-const app = Fastify({
-  logger: true,
-}).register(cors, {
-  origin: 'https://tasks-project-alpha.vercel.app/'
-});
-app.register(UserRoute)
-app.register(TaskRoute)
-
-app.get('/', async (req, reply) => {
+fastify.get('/', async (req, reply) => {
   return reply.send('hello')
 })
 
 export default async function handler(req, reply) {
-  await app.ready()
-  app.server.emit('request', req, reply)
+  await fastify.ready()
+  fastify.server.emit('request', req, reply)
 }
 
