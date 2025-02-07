7ebc9113edfa66e4ef20924477affb0f49ed4ddf
Jose Italo <122283414+JoseItaloP@users.noreply.github.com>
2025-02-07 16:41:47 -0300

    teste cors

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -1,29 +1,19 @@
 
 const fastify = require('fastify')({logger: true})
-
+fastify.register(require('@fastify/cors'), {
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+});
 fastify.register(require('./route/user'))
 fastify.register(require('./route/task'))
 
 fastify.get('/', async (req, reply) => {
-
-  reply.setHeader('Access-Control-Allow-Origin', '*')
-  reply.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
   return reply.send('hello')
 })
 
 export default async function handler(req, reply) {
-  reply.setHeader('Access-Control-Allow-Origin', '*')
-  reply.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
+  
   await fastify.ready()
   fastify.server.emit('request', req, reply)
 }
