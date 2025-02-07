f5d6c5f1fe631d90d481c80c3a0a27192b078a50
Jose Italo <122283414+JoseItaloP@users.noreply.github.com>
2025-02-07 16:21:59 -0300

    test cors resolution

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -1,7 +1,7 @@
 
 const fastify = require('fastify')({logger: true})
 
-fastify.register(require('./route/user'))
+fastify.register(require('./route/user'),{Headers: 'Access-Control-Allow-Origin: *'})
 fastify.register(require('./route/task'))
 
 fastify.get('/', async (req, reply) => {
@@ -13,7 +13,12 @@ export default async function handler(req, reply) {
  
   reply.setHeader('Access-Control-Allow-Origin', 'https://tasks-project-alpha.vercel.app')
   reply.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+
   fastify.server.emit('request', req, reply)
 }
 
