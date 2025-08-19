09bf59d82de42c0d42a38efc8cb3997ef839b242
Jose Italo <122283414+JoseItaloP@users.noreply.github.com>
2025-08-19 10:05:44 -0300

    test

--- BackEnd/api/index.ts
+++ BackEnd/api/index.ts
@@ -1,55 +1,64 @@
 import { fastify, FastifyReply, FastifyRequest } from 'fastify';
-import { fastifySwagger } from '@fastify/swagger'
-import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastifySwagger } from '@fastify/swagger';
+import { fastifySwaggerUi } from '@fastify/swagger-ui';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
 import UserRoute from './route/user';
 import TaskRoute from './route/task';
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+let app: any;
 
-app.setValidatorCompiler(validatorCompiler)
-app.setSerializerCompiler(serializerCompiler)
-
-app.addHook('onRequest', (request, reply, done) => {
-  reply.header('Access-Control-Allow-Origin', `${process.env.FRONT_URL}`);
-  reply.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  reply.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  reply.header('Access-Control-Allow-Credentials', 'true');
-  done();
-});
-
-
-app.options('*', (req, reply) => {
-  reply.status(204).send();
-});
-
-app.register(UserRoute);
-app.register(TaskRoute);
-
-app.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: 'ToDo List API',
-      version: '1.0.0'
-    }
+async function build() {
+  if (app) {
+    return app;
   }
-})
 
-app.register(fastifySwaggerUi, {
-  routePrefix: '/docs'
-})
+  const newApp = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.get('/', async (req, reply) => {
-  return reply.send('hello');
-});
+  newApp.setValidatorCompiler(validatorCompiler);
+  newApp.setSerializerCompiler(serializerCompiler);
 
+  newApp.addHook('onRequest', (request, reply, done) => {
+    reply.header('Access-Control-Allow-Origin', `${process.env.FRONT_URL}`);
+    reply.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    reply.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    reply.header('Access-Control-Allow-Credentials', 'true');
+    done();
+  });
 
+  newApp.options('*', (req, reply) => {
+    reply.status(204).send();
+  });
 
-exports = async (req: FastifyRequest, res: FastifyReply) => {
-  await app.ready();
-  app.server.emit('request', req, res);
+  newApp.register(UserRoute);
+  newApp.register(TaskRoute);
+
+  newApp.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: 'ToDo List API',
+        version: '1.0.0'
+      }
+    }
+    });
+
+  newApp.register(fastifySwaggerUi, {
+    routePrefix: '/docs'
+    });
+
+  newApp.get('/', async (req, reply) => {
+    return reply.send('Hello, world!');
+  });
+
+  await newApp.ready();
+  app = newApp;
+
+  return app;
 }
 
 
+export default async function handler(req: FastifyRequest, res: FastifyReply) {
+  const fastifyApp = await build();
+  fastifyApp.server.emit('request', req, res);
+}
